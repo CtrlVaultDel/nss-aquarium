156f20d12c3ac9fb46623818738a89782eea4956
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2020-11-17 16:27:44 -0600

    added and organized scripts folder

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 // Any JS that needs to be pushed when the page loads should be inside of main.js
 
-import { FishList } from "./FishList.js";
+import { FishList } from "./fish/FishList.js";
 
 // Immediately call the FishList function when the page loads
 FishList();
