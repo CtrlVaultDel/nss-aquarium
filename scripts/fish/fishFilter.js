7d7d1c902b237595395dcea2ec41794ac852c9a6
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2020-11-19 11:22:48 -0600

    Changed for loops in fishFilter.js to for..of method

--- scripts/fish/fishFilter.js
+++ scripts/fish/fishFilter.js
@@ -1,9 +1,9 @@
 export const filterHolyFish = (fishToFilter) => {
     // 3, 6, 9, 12, etc... fish
     const holyFish = []
-    for(let i = 0; i < fishToFilter.length; i++){
-        if((fishToFilter[i].size % 3) === 0){
-            holyFish.push(fishToFilter[i]);
+    for(const fish of fishToFilter){
+        if((fish.size % 3) === 0){
+            holyFish.push(fish);
         }
     }
     
@@ -13,9 +13,9 @@ export const filterHolyFish = (fishToFilter) => {
 export const filterSoldierFish = (fishToFilter) => {
     // 5, 10, 15, 20, 25, etc... fish
     const soldierFish = [];
-    for(let i = 0; i < fishToFilter.length; i++){
-        if((fishToFilter[i].size % 5) === 0){
-            soldierFish.push(fishToFilter[i]);
+    for(const fish of fishToFilter){
+        if((fish.size % 5) === 0){
+            soldierFish.push(fish);
         }
     }
     return soldierFish;
@@ -24,9 +24,9 @@ export const filterSoldierFish = (fishToFilter) => {
 export const filterNormalFish = (fishToFilter) => {
     // Any fish not a multiple of 3 or 5
     const regularFish = [];
-    for(let i = 0; i < fishToFilter.length; i++){
-        if((fishToFilter[i].size % 3) !== 0 && (fishToFilter[i].size % 5) !== 0){
-            regularFish.push(fishToFilter[i]);
+    for(const fish of fishToFilter){
+        if((fish.size % 3) !== 0 && (fish.size % 5) !== 0){
+            regularFish.push(fish);
         }
     }
     return regularFish;
